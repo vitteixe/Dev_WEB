6febd50fb761c9a34ab3f3ef060579a460bd09a6
Vitor Teixeira <vitoroliv@outlook.com>
2022-03-07 10:53:22 -0300

    variaveis

---
+++ JS-JavaScript/variaveis/variaveis.js
@@ -0,0 +1,13 @@
+//String
+var texto = 'Curso de JS'
+
+//number
+var numeroInt = -7
+var numeroFloat = 10.75
+
+//boolean
+var teste = false //true \ false
+
+//alert(numeroFloat) //abrir um dialogo
+//document.write(texto)
+console.log(numeroInt)
