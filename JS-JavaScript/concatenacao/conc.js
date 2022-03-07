7ed876ac4aadb2d699eafef8fad9a0e1d5c7f6b6
Vitor Teixeira <vitoroliv@outlook.com>
2022-03-07 11:29:24 -0300

    Concatenação

---
+++ JS-JavaScript/concatenacao/conc.js
@@ -0,0 +1,4 @@
+var nome = prompt('digite o seu nome: ');
+var idade = prompt('digite a sua idade: ');
+
+document.write('<h1>Olá ' + nome + ', tudo bem? Estou vendo aqui que você possui ' + idade + ' anos </h1>');
