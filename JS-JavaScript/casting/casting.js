f2d46f067d39b50622f143bf71f4ba22c55fe9c4
Vitor Teixeira <vitoroliv@outlook.com>
2022-03-14 10:28:39 -0300

    Mudando valores de variáveis - JS

---
+++ JS-JavaScript/casting/casting.js
@@ -0,0 +1,8 @@
+var variavell = 10;
+var variavel2 = 20;
+
+// variavell = parseInt(variavell); 
+// variavel2 = parseFloat(variavel2); 
+
+document.write(variavell.toString() + variavel2.toString());
+
