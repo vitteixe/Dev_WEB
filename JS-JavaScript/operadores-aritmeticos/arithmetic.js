19f7e77f1b10e4b6bb6db05fd8331d3e94375cea
Vitor Teixeira <vitoroliv@outlook.com>
2022-03-16 11:58:36 -0300

    Aritméticas2 - JS

--- JS-JavaScript/operadores-aritmeticos/arithmetic.js
+++ JS-JavaScript/operadores-aritmeticos/arithmetic.js
@@ -1,8 +1,10 @@
+/*
 var num1 = prompt('digite o num1');
 var num2 = prompt('digite o num2');
 
 num1 = parseInt(num1);
 num2 = parseInt(num2);
+
 
 document.write('A soma ente ' + num1 + ' e ' + num2 + ' é: '   + (num1 + num2) + '<br><br>');
 document.write('A subtração ente ' + num1 + ' e ' + num2 + ' é: '   + (num1 - num2) + '<br><br>');
@@ -11,3 +13,13 @@ document.write('A divisão ente ' + num1 + ' e ' + num2 + ' é: '   + (num1 / nu
 document.write('O módulo ente ' + num1 + ' e ' + num2 + ' é: '   + (num1 % num2) + '<br><br>');
 document.write('O incremento de ' + num1 + ' é ' + (++num1) + '<br><br>');
 document.write('O decremento de ' + num2 + ' é ' + (--num2) + '<br><br>');
+*/
+
+var teste = 10;
+
+teste += 5;
+//teste -= 5;
+//teste *= 5;
+//teste /= 2;
+//teste %= 3;
+document.write(teste);
