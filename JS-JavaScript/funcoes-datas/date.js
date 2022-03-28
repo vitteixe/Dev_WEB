02d32c1bf6ac0ad255e0bd2d95b72d3febc4dcf1
Vitor Teixeira <vitoroliv@outlook.com>
2022-03-28 10:34:40 -0300

    Funções de Datas - JS

---
+++ JS-JavaScript/funcoes-datas/date.js
@@ -0,0 +1,8 @@
+var data = new Date();
+
+document.write(data.getDate() + '/' + (data.getMonth() + 1) + '/' + data.getFullYear() + '_' + data.getHours()+'horas');
+
+
+
+
+//doc = https://www.w3schools.com/jsref/jsref_obj_date.asp
