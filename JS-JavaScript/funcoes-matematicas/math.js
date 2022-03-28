e773e4aefa581cbf523709e9effa21229885e638
Vitor Teixeira <vitoroliv@outlook.com>
2022-03-28 10:19:22 -0300

    Funções Matemáticas - JS

---
+++ JS-JavaScript/funcoes-matematicas/math.js
@@ -0,0 +1,13 @@
+var x = Math.round(10.680);
+
+document.write('<br> x = ' + x);
+
+
+var y = Math.floor(10.480);
+
+document.write('<br><br> y = ' + y);
+
+
+
+
+//doc = https://www.w3schools.com/jsref/jsref_obj_math.asp
