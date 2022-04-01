cb09e647937bd3a8c0e5d4b182886b583b898f96
Vitor Teixeira <vitoroliv@outlook.com>
2022-04-01 12:58:39 -0300

    Eventos mouse - JS

---
+++ JS-JavaScript/eventos/mouse/mouse.js
@@ -0,0 +1,12 @@
+function acao(){
+    alert('Evento disparado!')
+}
+
+
+function acao1(){
+    alert('O elemento foi clicado!');
+}
+
+function acao2(){
+    alert('O cursor do mouse deixou a região do elemento');
+}
