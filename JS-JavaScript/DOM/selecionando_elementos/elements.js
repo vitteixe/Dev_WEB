0b6eebd6c9de6fd640ef1fe5b40f9912b3696000
Vitor Teixeira <vitoroliv@outlook.com>
2022-04-01 16:32:41 -0300

    DOM Selecionando ekementos da página - JS

---
+++ JS-JavaScript/DOM/selecionando_elementos/elements.js
@@ -0,0 +1,13 @@
+/*
+getElementsByTagName();       TAG
+getElementsByClassName();     Class
+getElementsByName();          Name
+*/
+
+// console.log(document.getElementById('user'));
+
+// console.log(document.getElementsByTagName('input'));
+
+// console.log(document.getElementsByClassName('campo'));
+
+console.log(document.getElementsByName('nome'));
