4e5b6d1b5f12cfc6397969646fc0669e47092bfe
Vitor Teixeira <vitoroliv@outlook.com>
2022-04-11 09:36:29 -0300

    atv-JS

--- JS-JavaScript/tests/atv1/atv1.js
+++ JS-JavaScript/tests/atv1/atv1.js
@@ -5,10 +5,14 @@ var num1 = prompt('Digite o primeiro número: ');
 var num2 = prompt('Digite o segundo número: ');
 
 
-var equality = (num1 == num2)
-
-var identical = (num1 === num2)
-
+var equality = (num1 == num2);
+var identical = (num1 === num2);
+var different = (num1 != num2);
+var notIdentical = (num1 !== num2);
+var smaller = (num1 < num2);
+var bigger = (num1 > num2);
+var smallerEquality = (num1 <= num2);
+var biggerEquality = (num1 >= num2);
 
 var button = document.getElementById ('comparar');
 
@@ -20,6 +24,13 @@ button.addEventListener("click", function() {
 
     document.getElementById("igualdade").value = equality;
     document.getElementById("identico").value = identical;
+    document.getElementById("diferente").value = different;
+    document.getElementById("NIdentico").value = notIdentical;
+    document.getElementById("menor").value = smaller;
+    document.getElementById("maior").value = bigger;
+    document.getElementById("menori").value = smallerEquality;
+    document.getElementById("maiori").value = biggerEquality;
+    
 
     if(aluno.style.display === "none") {
         aluno.style.display = "block";
