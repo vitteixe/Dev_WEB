b90fb839581c95ef66ea671afd7c8cb62a6d029e
Vitor Teixeira <vitoroliv@outlook.com>
2022-04-11 08:49:25 -0300

    DOM Manipulando elementos - JS

---
+++ JS-JavaScript/DOM/manipulando_estilos/elements.js
@@ -0,0 +1,6 @@
+function modificaEstilo(corDeFundo, l, a){
+
+    document.getElementById('quadrado').style.backgroundColor = corDeFundo;
+    document.getElementById('quadrado').style.width = l;
+    document.getElementById('quadrado').style.height = a;
+}
