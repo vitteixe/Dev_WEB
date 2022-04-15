99d38a5a1ff8ee696d723f78544a35316291da24
Vitor Teixeira <vitoroliv@outlook.com>
2022-04-15 09:38:03 -0300

    projeto-9 Calculadora -  Implementando a função calcular ( )

--- Projetos/projeto-9/js/calc.js
+++ Projetos/projeto-9/js/calc.js
@@ -1,3 +1,25 @@
 function calcular(tipo, valor){
-    console.log(tipo, valor)
+    
+
+    if (tipo === 'acao'){
+
+        if(valor === 'c'){
+            //limpar o visor (id results)
+            document.getElementById('results').value = '';
+        }
+
+        if(valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.'){
+            document.getElementById('results').value += valor;
+        }
+
+        if(valor === '='){
+            var valor_camp = eval(document.getElementById('results').value)
+
+            document.getElementById('results').value = valor_camp;
+        }
+
+    } else if (tipo === 'valor'){
+
+        document.getElementById('results').value += valor;
+    }
 }
