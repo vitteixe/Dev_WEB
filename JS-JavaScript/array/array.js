5eef95b809e8860f71f2ef3fa69f2cd4a05bac6b
Vitor Teixeira <vitoroliv@outlook.com>
2022-04-25 10:09:17 -0300

    Array - JS

---
+++ JS-JavaScript/array/array.js
@@ -0,0 +1,15 @@
+//var lista_frutas = Array();
+/*
+var lista_frutas = [];
+
+lista_frutas[1] = 'Banana';
+lista_frutas[2] = true;
+lista_frutas['x'] = 'Morango';
+lista_frutas[4] = 'Uva';
+lista_frutas[5] = 100;
+*/
+
+//var lista_frutas = Array('Banana', 'Maça', 'Morango', 'uva');
+var lista_frutas = ['Banana', 'Maça', 'Morango', 'uva'];
+
+console.log(lista_frutas);
