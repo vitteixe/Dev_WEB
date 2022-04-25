fcd5f214e16c2f29b39bf4754e191ced9ff26543
Vitor Teixeira <vitoroliv@outlook.com>
2022-04-25 11:25:27 -0300

    Array Multidimensional - JS

---
+++ JS-JavaScript/array-multidimensional/multidimensional.js
@@ -0,0 +1,13 @@
+var lista_coisas = [];
+
+lista_coisas ['Frutas'] = ['Banana', 'Maça', 'Morango', 'uva'];
+
+
+lista_coisas['Pessoas'] = Array();
+
+lista_coisas['Pessoas']['a'] = 'Vitor';
+lista_coisas['Pessoas']['b'] = 'Julia';
+lista_coisas['Pessoas']['c'] = 'Paulo';
+
+
+document.write(lista_coisas['Pessoas']['b']);
