18cd0f3d241aa2832bc78bbf81915e4acb88883c
Vitor Teixeira <vitoroliv@outlook.com>
2022-05-16 10:27:19 -0300

    Estrutura de repetição [pratica-1] - JS

---
+++ JS-JavaScript/estrturas-de-repeticao/pratica-1/practical1.js
@@ -0,0 +1,13 @@
+var lista_frutas = [];
+
+lista_frutas[0] = 'Banana';
+lista_frutas[1] = 'Maçã';
+lista_frutas[2] = 'Morango';
+lista_frutas[3] = 'Uva';
+
+var y = 0;
+
+while(y < lista_frutas.length){
+    document.write(lista_frutas[y]+'<br>')
+    y++
+}
