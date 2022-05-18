5f41b271e792f648eed90c96210551c5db9290e8
Vitor Teixeira <vitoroliv@outlook.com>
2022-05-18 09:45:05 -0300

    Estrutura de repetição [pratica-2] - JS

---
+++ JS-JavaScript/estrturas-de-repeticao/pratica-2/practical2.js
@@ -0,0 +1,13 @@
+for(var y = 1; y<=10; y++){
+
+    for(var x = 1; x <= 10; x++){
+        document.write(y + " x " + x + " = " + (y*x) + '<br>');
+    }
+
+    document.write('<hr>');
+    
+}
+
+
+
+
