655845031f4ed480c9f0c890feb0547105333660
Vitor Teixeira <vitoroliv@outlook.com>
2022-05-18 10:53:34 -0300

    Teste 7 - JS

---
+++ JS-JavaScript/tests/teste7/test7.js
@@ -0,0 +1,10 @@
+var lista_frutas = [];
+
+lista_frutas[0] = 'Banana';
+lista_frutas[1] = 'Maçã';
+lista_frutas[2] = 'Morango';
+lista_frutas[3] = 'Uva';
+
+for(i=0; i<lista_frutas.length; i++){
+    document.write(lista_frutas[i]+'<br>')
+}
