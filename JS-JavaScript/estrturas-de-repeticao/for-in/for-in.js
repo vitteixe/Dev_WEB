3eee75b4f4add41ba69d6f51ee178b543706168f
Vitor Teixeira <vitoroliv@outlook.com>
2022-05-18 10:07:08 -0300

    (For-in) Estrutura de repetição - JS

---
+++ JS-JavaScript/estrturas-de-repeticao/for-in/for-in.js
@@ -0,0 +1,7 @@
+listaConvidados = ['Jorge','Vitor','Julia','Paulo','Monica'];
+
+console.log(listaConvidados);
+
+for (var x in listaConvidados) {
+    document.write('Índice ' + x + ' valor ' + listaConvidados[x] + '<br>');
+}
