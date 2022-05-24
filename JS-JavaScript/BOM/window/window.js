c46b7d170b6bc56e26c26d6750bba36f8daf4fac
Vitor Teixeira <vitoroliv@outlook.com>
2022-05-24 12:09:01 -0300

    BOM Window - JS

---
+++ JS-JavaScript/BOM/window/window.js
@@ -0,0 +1,10 @@
+var janela 
+
+function abrirPopUp(){
+    janela = window.open('http://www.google.com.br', 'nova_janela', 'width=250 , height=500');
+
+}
+
+function fecharPopUp(){
+    janela.close()
+}
