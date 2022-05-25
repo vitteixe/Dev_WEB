bf93331cdb7363d8fa2d311dd482e74e81f42540
Vitor Teixeira <vitoroliv@outlook.com>
2022-05-25 11:16:15 -0300

    projeto-10 Game Mata Mosquito - Definindo as dimensões do jogo

---
+++ Projetos/projeto-10/js/game.js
@@ -0,0 +1,12 @@
+var height;
+var width;
+
+function ajustaTamanhoPalcoJogo(){
+    height = window.innerHeight;
+    width = window.innerWidth;  
+
+    console.log(height,width);
+}
+
+ajustaTamanhoPalcoJogo()
+
