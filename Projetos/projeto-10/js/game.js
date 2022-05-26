9daf268b82b0ef4d8434db3bcda8b2fcb84ab8b0
Vitor Teixeira <vitoroliv@outlook.com>
2022-05-26 10:41:04 -0300

    projeto-10 Game Mata Mosquito - Criando posições randômicas

--- Projetos/projeto-10/js/game.js
+++ Projetos/projeto-10/js/game.js
@@ -8,5 +8,24 @@ function ajustaTamanhoPalcoJogo(){
     console.log(height,width);
 }
 
-ajustaTamanhoPalcoJogo()
+ajustaTamanhoPalcoJogo();
 
+// Posições aleatórias
+var positionX = Math.floor(Math.random() * width) - 90; // transformar em número decimal .floor
+var positionY = Math.floor(Math.random() * height) - 90;
+
+positionX = positionX < 0 ? 0 : positionX; //se posição for menor que zero; se for recebe 0; se não for recebe ela mesma
+positionY = positionY < 0 ? 0 : positionY;
+
+console.log(positionX, positionY);
+
+//criar elemento html
+var mosquito = document.createElement('img');
+mosquito.src = 'images/mosquito.png';
+mosquito.className = 'mosquito1';
+mosquito.style.position = 'absolute'
+mosquito.style.left = positionX + 'px';
+mosquito.style.top = positionY + 'px';
+
+//adicionar 'mosquito' como filho do body
+document.body.appendChild(mosquito);
