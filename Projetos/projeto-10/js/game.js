639c6997f5a181be0d4d573225d993a043c03ae4
Vitor Teixeira <vitoroliv@outlook.com>
2022-05-26 11:47:30 -0300

    projeto-10 Game Mata Mosquito - Criando e removendo mosquito a cada ciclo de tempo

--- Projetos/projeto-10/js/game.js
+++ Projetos/projeto-10/js/game.js
@@ -1,6 +1,16 @@
+ajustaTamanhoPalcoJogo();
+
+//inserir elemento (mosquito) a cada 1seg
+setInterval(function(){
+    posicaoRnadomica();
+}, 1000);
+
+
+
 //Capiturando Altura e Largura do objeto Window.
 var height;
 var width;
+
 
 function ajustaTamanhoPalcoJogo(){
     height = window.innerHeight;
@@ -9,28 +19,36 @@ function ajustaTamanhoPalcoJogo(){
     console.log(height,width);
 }
 
-ajustaTamanhoPalcoJogo();
 
-// Posições aleatórias
-var positionX = Math.floor(Math.random() * width) - 90; // transformar em número decimal .floor
-var positionY = Math.floor(Math.random() * height) - 90;
+function posicaoRnadomica(){
 
-positionX = positionX < 0 ? 0 : positionX; //se posição for menor que zero; se for recebe 0; se não for recebe ela mesma
-positionY = positionY < 0 ? 0 : positionY;
+    //remover mosquito anterior (caso exista)
+    if(document.getElementById('mosquito')){
+        document.getElementById('mosquito').remove();
+    }
 
-console.log(positionX, positionY);
+    // Posições aleatórias
+    var positionX = Math.floor(Math.random() * width) - 90; // transformar em número decimal .floor
+    var positionY = Math.floor(Math.random() * height) - 90;
 
-//criar elemento html
-var mosquito = document.createElement('img');
-mosquito.src = 'images/mosquito.png';
-mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio();
-mosquito.style.position = 'absolute'
-mosquito.style.left = positionX + 'px';
-mosquito.style.top = positionY + 'px';
+    positionX = positionX < 0 ? 0 : positionX; //se posição for menor que zero; se for recebe 0; se não for recebe ela mesma
+    positionY = positionY < 0 ? 0 : positionY;
 
-//adicionar 'mosquito' como filho do body
-document.body.appendChild(mosquito);
+    console.log(positionX, positionY);
+    
+    //criar elemento html
+    var mosquito = document.createElement('img');
+    mosquito.src = 'images/mosquito.png';
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio();
+    mosquito.style.position = 'absolute'
+    mosquito.style.left = positionX + 'px';
+    mosquito.style.top = positionY + 'px';
+    mosquito.id = 'mosquito'
 
+    //adicionar 'mosquito' como filho do body
+    document.body.appendChild(mosquito);
+
+}
 
 
 //função para classes de tamanhos diferentes
