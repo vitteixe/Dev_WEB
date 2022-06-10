bf6117a39d33e6cc9fa2d643499fa28f6d6f61a1
Vitor Teixeira <vitoroliv@outlook.com>
2022-06-10 10:41:23 -0300

    LTB - ATUALIZAÇÕES

--- Projetos/LTB/js/menu.js
+++ Projetos/LTB/js/menu.js
@@ -1,14 +1,53 @@
 const btnMobile = document.getElementById('btn-mobile');
+const header = document.querySelector('header');
 
 function toggleMenu(event){
     if(event.type ==='touchstart') event.preventDefault()
 
-    const header = document.querySelector('header');
     header.classList.toggle('active');
 
     const nav = document.getElementById('nav');
-    nav.classList.toggle('active');
+    nav.classList.add('active');
 }
 
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
+
+
+//////////////////////////////////////////////////////////////
+
+
+const positions =  setInterval(function () {
+    
+    const positionY = header.getBoundingClientRect().y*(-1);
+    const menuFixed = document.querySelector('#h-fixed');
+
+    width = window.innerWidth;  
+
+    if(width <= 979){
+        
+        if(positionY >= 380){
+            menuFixed.classList.add('active');
+        }else{
+            menuFixed.classList.remove('active');
+        }
+        
+    } else if(width <= 991){
+
+        if(positionY >= 555){
+            menuFixed.classList.add('active');
+        }else{
+            menuFixed.classList.remove('active');
+        }
+
+    } else{
+
+        if(positionY >= 804){
+            menuFixed.classList.add('active');
+        }else{
+            menuFixed.classList.remove('active');
+        }
+
+    }
+
+})
