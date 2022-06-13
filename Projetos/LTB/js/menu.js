db6c24014bb315253408bbf4a3330870b33bd900
Vitor Teixeira <vitoroliv@outlook.com>
2022-06-13 12:10:26 -0300

    LTB

--- Projetos/LTB/js/menu.js
+++ Projetos/LTB/js/menu.js
@@ -1,13 +1,13 @@
 const btnMobile = document.getElementById('btn-mobile');
 const header = document.querySelector('header');
+const nav = document.getElementById('nav');
 
 function toggleMenu(event){
     if(event.type ==='touchstart') event.preventDefault()
 
     header.classList.toggle('active');
 
-    const nav = document.getElementById('nav');
-    nav.classList.add('active');
+    nav.classList.toggle('active');
 }
 
 btnMobile.addEventListener('click', toggleMenu);
@@ -21,33 +21,51 @@ const positions =  setInterval(function () {
     
     const positionY = header.getBoundingClientRect().y*(-1);
     const menuFixed = document.querySelector('#h-fixed');
+    const width = window.innerWidth;  
+    
+    const active = nav.classList.contains('active');
 
-    width = window.innerWidth;  
+    
 
-    if(width <= 979){
-        
-        if(positionY >= 380){
-            menuFixed.classList.add('active');
-        }else{
-            menuFixed.classList.remove('active');
-        }
-        
-    } else if(width <= 991){
+    if(width <= 979 && active){
 
-        if(positionY >= 555){
+        if(positionY >= 587){
             menuFixed.classList.add('active');
         }else{
             menuFixed.classList.remove('active');
         }
 
-    } else{
+    } else {
 
-        if(positionY >= 804){
-            menuFixed.classList.add('active');
-        }else{
-            menuFixed.classList.remove('active');
+        if(width <= 979){
+        
+            if(positionY >= 380){
+                menuFixed.classList.add('active');
+            }else{
+                menuFixed.classList.remove('active');
+            }
+            
+        } else if(width <= 991){
+        
+            if(positionY >= 555){
+                menuFixed.classList.add('active');
+            }else{
+                menuFixed.classList.remove('active');
+            }
+        
+        } else{
+        
+            if(positionY >= 804){
+                menuFixed.classList.add('active');
+            }else{
+                menuFixed.classList.remove('active');
+            }
+        
         }
 
     }
+    
+
+    
 
 })
