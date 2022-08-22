4dd37406bdc8166611d683328b5c20e4e123680f
Vitor Teixeira <vitoroliv@outlook.com>
2022-08-22 10:30:35 -0300

    projeto-11 Capturando objetos filtrados

--- Projetos/projeto-11/js/app.js
+++ Projetos/projeto-11/js/app.js
@@ -76,7 +76,42 @@ class Bd {
     }
 
     pesquisar(despesa){
-        console.log(despesa)
+
+        let despesasFiltradas = [];
+
+        despesasFiltradas = this.recuperarTodosRegistros();
+
+        //ano
+        if(despesa.ano != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.ano == despesa.ano);
+        }
+
+        //mes
+        if(despesa.mes != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.mes == despesa.mes);
+        }
+
+        //dia
+        if(despesa.dia != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.dia == despesa.dia);
+        }
+
+        //tipo
+        if(despesa.tipo != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.tipo == despesa.tipo);
+        }
+
+        //descricao
+        if(despesa.descricao != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.descricao == despesa.descricao);
+        }
+
+        //valor
+        if(despesa.valor != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.valor == despesa.valor);
+        }
+
+        console.log(despesasFiltradas)
     }
 
 }
