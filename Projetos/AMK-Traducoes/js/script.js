19f13303ae0462bb560288c5981f9e0eac385457
Vitor Teixeira <vitoroliv@outlook.com>
2022-09-02 17:21:28 -0300

    AMK - Traduções Juramentadas (Finalização da recriação do layout)

--- Projetos/AMK-Traducoes/js/script.js
+++ Projetos/AMK-Traducoes/js/script.js
@@ -12,3 +12,26 @@ toggleMenu = (event) => {
 
 btn.addEventListener('click', toggleMenu);
 btn.addEventListener('touchstart', toggleMenu);
+
+const dataAnimated = document.querySelectorAll('[data-animated]')
+const classAnimated = 'animate';
+
+scrollAnimate = () =>{
+    
+    const windowTop = window.pageYOffset + ((window.innerHeight) * 3 / 4);
+    
+    dataAnimated.forEach(function(element){
+
+        if(windowTop > element.offsetTop){
+            element.classList.add(classAnimated);
+        } else{
+            element.classList.remove(classAnimated);
+        }
+
+    })
+
+    console.log(windowTop);
+
+}
+
+window.addEventListener('scroll', scrollAnimate);
