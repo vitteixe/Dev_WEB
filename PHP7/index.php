2e517d3054d76dcd15389c5740efd031086727e7
Vitor Teixeira <vitoroliv@outlook.com>
2022-09-06 10:48:02 -0300

    Embutindo blocos PHP em HTML - PHP

---
+++ PHP7/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>PHP</title>
+
+</head>
+<body>
+    
+    <?php 
+        echo 'Hello world! <br>';
+    ?>
+
+    <?= 'Vamos iniciar o curso!!!!'?>
+
+</body>
+</html>
