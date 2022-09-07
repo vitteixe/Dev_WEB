ede732c6893cf19c186ac0e5b92537297c7a0518
Vitor Teixeira <vitoroliv@outlook.com>
2022-09-07 20:24:31 -0300

    Comentários - PHP7

---
+++ PHP7/comentarios/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Comentários - PHP</title>
+
+</head>
+<body>
+    
+    <!----------------------------------
+    |                             |
+    |    VITTEIXE PRODUCTION      |
+    |           ᓚᘏᗢ              |
+    |                             |
+    ------------------------------->
+    
+    <?php 
+        // Comentário de uma linha
+        echo '<h1>Comando echo</h1>';
+
+        # comentário de uma linha
+        print '<h1>Comando print</h1><hr>';
+
+        /*
+            Comentário em bloco 
+            (múltiplas linhas).
+            Este tipo de comentário serve para descrever em 
+            diversas linhas
+        */
+    ?>
+
+</body>
+</html>
