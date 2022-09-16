64ea973b160c7058ad52b1a5f0bcd788c3e09151
Vitor Teixeira <vitoroliv@outlook.com>
2022-09-16 20:40:51 -0300

    Cadastro de despesas - Projeto Integrador

--- Projetos/INT/js/script.js
+++ Projetos/INT/js/script.js
@@ -6,3 +6,33 @@ function toggleMenu(){
 }
 
 btnMobile.addEventListener('click', toggleMenu);
+
+
+//---------------------------------------------------------------------//
+const btnRegister = document.querySelector('#btn-register');
+const btnSearch = document.querySelector('#btn-search');
+
+
+RegisterExpense = () => {
+    
+    let year = document.querySelector('#year');
+    let month = document.querySelector('#month');
+    let day = document.querySelector('#day');
+    let type = document.querySelector('#type');
+    let description = document.querySelector('#description');
+    let value = document.querySelector('#value');
+
+    console.log(year.value, month.value, day.value, type.value, description.value, value.value)
+
+}
+
+
+
+if(document.body.contains(btnRegister)){
+    btnRegister.addEventListener('click', RegisterExpense);
+} else if(document.body.contains(btnSearch)){
+    btnSearch.addEventListener('click', RegisterExpense);
+}
+
+
+
