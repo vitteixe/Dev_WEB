b4a250d23a6a1d27ec62b931e8ce4697f6590daf
Vitor Teixeira <vitoroliv@outlook.com>
2022-09-28 20:15:51 -0300

    Operações Aritméticas com Atribuições - PHP

---
+++ PHP7/operacoes-aritmeticas/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operações Aritméticas - PHP</title>
+</head>
+<body>
+    
+    <?php 
+
+        $x = 10;
+        $y = 8;
+
+        $x %= $y;
+
+        echo $x;
+        
+    ?>
+
+
+</body>
+</html>
