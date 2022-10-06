ab31b41ea9066786135da6bdeafea2ad2be1d571
Vitor Teixeira <vitoroliv@outlook.com>
2022-10-06 17:05:35 -0300

    App Help Desk - Projeto 12

---
+++ Projetos/projeto-12/index.php
@@ -0,0 +1,49 @@
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>App Help Desk</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <style>
+      .card-login {
+        padding: 30px 0 0 0;
+        width: 350px;
+        margin: 0 auto;
+      }
+    </style>
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">
+        <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+        App Help Desk
+      </a>
+    </nav>
+
+    <div class="container">    
+      <div class="row">
+
+        <div class="card-login">
+          <div class="card">
+            <div class="card-header">
+              Login
+            </div>
+            <div class="card-body">
+              <form>
+                <div class="form-group">
+                  <input type="email" class="form-control" placeholder="E-mail">
+                </div>
+                <div class="form-group">
+                  <input type="password" class="form-control" placeholder="Senha">
+                </div>
+                <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
+              </form>
+            </div>
+          </div>
+        </div>
+    </div>
+  </body>
+</html>
---
+++ Projetos/projeto-12/php/consultar_chamado.php
@@ -0,0 +1,66 @@
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>App Help Desk</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <style>
+      .card-consultar-chamado {
+        padding: 30px 0 0 0;
+        width: 100%;
+        margin: 0 auto;
+      }
+    </style>
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">
+        <img src="../img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+        App Help Desk
+      </a>
+    </nav>
+
+    <div class="container">    
+      <div class="row">
+
+        <div class="card-consultar-chamado">
+          <div class="card">
+            <div class="card-header">
+              Consulta de chamado
+            </div>
+            
+            <div class="card-body">
+              
+              <div class="card mb-3 bg-light">
+                <div class="card-body">
+                  <h5 class="card-title">Título do chamado...</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
+                  <p class="card-text">Descrição do chamado...</p>
+
+                </div>
+              </div>
+
+              <div class="card mb-3 bg-light">
+                <div class="card-body">
+                  <h5 class="card-title">Título do chamado...</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
+                  <p class="card-text">Descrição do chamado...</p>
+
+                </div>
+              </div>
+
+              <div class="row mt-5">
+                <div class="col-6">
+                  <button class="btn btn-lg btn-warning btn-block" type="submit">Voltar</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
