e52250de1959ea92f17f745708148e1a4dd33074
Vitor Teixeira <vitoroliv@outlook.com>
2022-10-20 20:53:08 -0300

    Getters e Setters mágicos (Overloading) - PHP OO

---
+++ PHP_OO/overloading/index.php
@@ -0,0 +1,44 @@
+<?php 
+
+    class Funcionario {
+
+        # atributos
+        public $nome = null;
+        public $telefone = null;
+        public $numFilhos = null;
+        public $cargo = null;
+        public $salario = null;
+
+
+        # Getters e Setters (overloading / sobrecarga)
+        function __set($atributo, $valor){
+            $this->$atributo = $valor;
+        }
+
+        function __get($attr){
+            return $this->$attr;
+        }
+
+    }
+
+    $y = new Funcionario(); 
+    $y->__set("nome", "Vitor");
+    $y->__set("telefone", "1194928-5568");
+    $y->__set("numFilhos", 2);
+    $y->__set("cargo", "Desenvolvedor Web");
+    $y->__set("salario", 2365.88);
+
+
+    echo $y->__get("nome") .  " possui " . $y->__get("numFilhos") . " filho(s) <br> Recebe " . $y->__get("salario") . " reais <br><br>";
+
+
+   
+    $x = new Funcionario();
+    $x->__set("nome", "Julia");
+    $x->__set("numFilhos", 0);
+
+    echo $x->__get("nome") .  " possui " . $x->__get("numFilhos") . " filho(s) <br><br>";
+
+    
+    
+?>
