49020d1bcb7609e4abcc0d12fe4ad31d73af0827
Vitor Teixeira <vitoroliv@outlook.com>
2022-12-06 16:16:40 -0300

    Adicionando a biblioteca PHPMailer - Projeto 13 Send Mail

--- Projetos/projeto-13/php/shipping-process.php
+++ Projetos/projeto-13/php/shipping-process.php
@@ -6,6 +6,19 @@
     ------------------------------->
 <?php
 
+    # Importando a biblioteca PHPMailer
+    require "../lib/PHPMailer/Exception.php";
+    require "../lib/PHPMailer/OAuth.php";
+    require "../lib/PHPMailer/PHPMailer.php";
+    require "../lib/PHPMailer/POP3.php";
+    require "../lib/PHPMailer/SMTP.php";
+    
+    # Usando namespaces da biblioteca
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+
+
     class Message{
         private $para = null;
         private $assunto = null;
@@ -40,10 +53,48 @@
     $mensagem->__set("mensagem", $_POST["message"]);
 
     # Convocando o método de validação de mensagem
-    if($mensagem->mensagemValida()){
-        echo "Enviando Mensagem!";
-    } else {
+    if(!$mensagem->mensagemValida()){
         echo "Erro ao enviar mensagem, existem campos inválidos";
+        die();
+    } 
+
+
+    $mail = new PHPMailer(true);
+
+    try {
+        //Server settings
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'user@example.com';                     //SMTP username
+        $mail->Password   = 'secret';                               //SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+        $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+        //Recipients
+        $mail->setFrom('from@example.com', 'Mailer');
+        $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
+        $mail->addAddress('ellen@example.com');               //Name is optional
+        $mail->addReplyTo('info@example.com', 'Information');
+        $mail->addCC('cc@example.com');
+        $mail->addBCC('bcc@example.com');
+
+        //Attachments
+        $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+        $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = 'Here is the subject';
+        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Não foi possível enviar esse e-mail, tente novamente mais tarde!";
+        echo "Detalhes do erro: {$mail->ErrorInfo}";
     }
 
 ?>
