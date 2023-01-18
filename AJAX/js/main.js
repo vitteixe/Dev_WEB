39b347a8216f38fbd3f11a4dc75248e046015641
Vitor Teixeira <vitoroliv@outlook.com>
2023-01-18 18:29:40 -0300

    Aplicando um loading - AJAX

--- AJAX/js/main.js
+++ AJAX/js/main.js
@@ -2,10 +2,22 @@ function requisitarPagina(url){
 
     let ajax = new XMLHttpRequest();
 
+    // Requisição não iniciada, state = 0
+    console.log(ajax.readyState) 
+    
     ajax.open("GET", url);
-    ajax.send();
 
-    console.log(ajax)
+    // conexão estabelecida com o servidor, state = 1
+    console.log(ajax.readyState)
+    
+
+    // lógica que fica olhando para o progresso da req
+    ajax.onreadystatechange = () => {
+        console.log(ajax.readyState)
+    }
+
+    ajax.send();
+    //console.log(ajax) 
 
 }
 
