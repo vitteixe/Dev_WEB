721b5a2b3e682e304e50942bde3a906ebb4cca6a
Vitor Teixeira <vitoroliv@outlook.com>
2023-01-19 18:57:22 -0300

    Aplicando um loading - AJAX

--- AJAX/js/main.js
+++ AJAX/js/main.js
@@ -1,19 +1,33 @@
 function requisitarPagina(url){
 
+    // incluir o gif de loading na página
+    if(!document.querySelector("#loading")){
+        let imgLoading = document.createElement("img");
+        imgLoading.id = "loading";
+        imgLoading.src = "../img/loading.gif";
+        imgLoading.className = "rounded mx-auto d-block";
+
+        document.querySelector("#conteudo").appendChild(imgLoading);
+    }
+
+    
     let ajax = new XMLHttpRequest();
 
     // Requisição não iniciada, state = 0
-    console.log(ajax.readyState) 
+    //console.log(ajax.readyState) 
     
     ajax.open("GET", url);
 
     // conexão estabelecida com o servidor, state = 1
-    console.log(ajax.readyState)
+    //.log(ajax.readyState)
     
 
     // lógica que fica olhando para o progresso da req
     ajax.onreadystatechange = () => {
-        console.log(ajax.readyState)
+        if(ajax.readyState == 4){
+            console.log("sucesso");
+            document.querySelector("#loading").remove();
+        }
     }
 
     ajax.send();
