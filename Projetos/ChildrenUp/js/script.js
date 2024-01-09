5de435adfeb2cad32e295f3077b56be4f11e5d2b
Vitor Teixeira <vitoroliv@outlook.com>
2024-01-09 16:36:21 -0300

    Correção no Header e acrescrentando link no Botão do Whatsapp - ChildUp

--- Projetos/ChildrenUp/js/script.js
+++ Projetos/ChildrenUp/js/script.js
@@ -8,3 +8,10 @@ function toggleMenu(){
 if(document.body.contains(menuFlex)){
     menuFlex.addEventListener("click", toggleMenu);
 }
+
+
+const btnWpp = document.querySelector("#Whatsapp");
+
+btnWpp.addEventListener("click", function(){
+    window.open("https://wa.me/32471369451?text=Bonjour,%20je%20souhaite%20planifierr%20une%20consultation!")
+})
